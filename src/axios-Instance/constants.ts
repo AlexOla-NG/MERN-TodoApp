a3b4be3a7ab2ea53a6bc78480d824e45d6a24d70
Alex Oweka <alexanderolalekanoweka@gmail.com>
2023-07-01 12:59:35 +0100

    🌱setup: project files, folders

---
+++ src/axios-Instance/constants.ts
@@ -0,0 +1,2 @@
+export const baseURL = "https://mern-recipe-app-api.vercel.app/";
+// export const baseURL = "http://localhost:3001/";
---
+++ src/react-query/constants.ts
@@ -0,0 +1,7 @@
+export const queryKeys = {
+	user: "user",
+	authentication: "authentication",
+	recipe: "recipe",
+	recipes: "recipes",
+	savedRecipes: "savedRecipes",
+};
