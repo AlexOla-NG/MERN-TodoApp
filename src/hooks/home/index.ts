d188110b23a918e3cbdc1a395e33576aa61452b2
Alx Oweka <alexanderolalekanoweka@gmail.com>
2023-08-29 21:58:34 +0100

    🎫ticket 1: refactor for pagination

--- src/hooks/home/index.ts
+++ src/hooks/home/index.ts
@@ -3,8 +3,12 @@ import { axiosInstance } from "../../axios-Instance";
 import { queryKeys } from "../../react-query/constants";
 import { errorAlert } from "../../utils";
 
+// TODO: refactor for pagination
+// replace useQuery with useMutation
+// update getDBTodos function to take params: page, limit
+
 const getDBTodos = async () => {
-	const res = await axiosInstance.get(`/todos`, {
+	const res = await axiosInstance.get(`/todos?page=1&limit=10`, {
 		headers: {
 			"Content-Type": "application/json",
 		},
