fa82ac1fc621366debf63a6507cf69b906a0d19f
Alx Oweka <alexanderolalekanoweka@gmail.com>
2023-09-01 05:29:32 +0100

    🔥feat: updated getDBTodosStats & getDBTodos calls

--- src/hooks/home/index.ts
+++ src/hooks/home/index.ts
@@ -1,14 +1,10 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { axiosInstance } from "../../axios-Instance";
 import { queryKeys } from "../../react-query/constants";
 import { errorAlert } from "../../utils";
 
-// TODO: refactor for pagination
-// replace useQuery with useMutation
-// update getDBTodos function to take params: page, limit
-
-const getDBTodos = async () => {
-	const res = await axiosInstance.get(`/todos?page=1&limit=10`, {
+const getDBTodosStats = async () => {
+	const res = await axiosInstance.get(`/todos`, {
 		headers: {
 			"Content-Type": "application/json",
 		},
@@ -17,14 +13,41 @@ const getDBTodos = async () => {
 	return res?.data?.data;
 };
 
-export const useGetDBTodos = () => {
+const getDBTodos = async (page: number, limit: number) => {
+	const res = await axiosInstance.get(`/todos?page=${page}&limit=${limit}`, {
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+
+	return res?.data?.data;
+};
+
+export const useGetDBTodosStats = () => {
 	const { isSuccess, data, isLoading } = useQuery({
 		queryKey: [queryKeys.dbTodos],
-		queryFn: () => getDBTodos(),
+		queryFn: () => getDBTodosStats(),
 		onError: (error) => {
 			errorAlert(error);
 		},
 	});
 
 	return { isSuccess, data, isLoading };
+};
+
+export type mutationFnProps = {
+	page: number;
+	limit: number;
+};
+
+export const useGetDBTodos = () => {
+	const { mutate, isSuccess, data, isLoading } = useMutation({
+		mutationFn: ({ page, limit }: mutationFnProps) =>
+			getDBTodos(page, limit),
+		onError: (error) => {
+			errorAlert(error);
+		},
+	});
+
+	return { mutate, isSuccess, data, isLoading };
 };
