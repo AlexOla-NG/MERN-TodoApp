d61397038b09ecac92b9176a0e773f3dabe9400d
Alx Oweka <alexanderolalekanoweka@gmail.com>
2023-09-02 13:08:24 +0100

    🔥feat: added loading skeleton to todocardlist

--- src/views/home/components/TodoCardList.tsx
+++ src/views/home/components/TodoCardList.tsx
@@ -1,21 +1,35 @@
-import React from 'react'
-import TodoCard from './TodoCard'
-import { dashboardProps } from './Dashboard'
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import TodoCard from "./TodoCard";
+import { dashboardProps } from "./Dashboard";
 
-const TodoCardList = ({dbTodos, isLoading}: dashboardProps) => {
-  return (
+const TodoCardList = ({ dbTodos, isLoading }: dashboardProps) => {
+	return (
 		<section className="todo-card-list">
 			<ul>
-				{dbTodos?.map((todo) => (
-					<TodoCard
-						key={todo._id}
-						todo={todo}
-						isLoading={isLoading}
-					/>
-				))}
+				{isLoading
+					? Array.from({ length: 5 }).map((_, index) => (
+							<li key={index}>
+								<Skeleton
+									baseColor="#202020"
+									highlightColor="#444"
+									height="140px"
+									width="100%"
+								/>
+							</li>
+						))
+					: dbTodos?.map((todo) => (
+							<TodoCard
+								key={todo._id}
+								todo={todo}
+								isLoading={isLoading}
+							/>
+						))
+				}
 			</ul>
 		</section>
-  );
-}
+	);
+};
 
-export default TodoCardList
+export default TodoCardList;
